fec320b8c5369d22ec26796b22afa8a7e3160efb
Manuel Strehl <git@manuel-strehl.de>
2010-12-22 07:57:14 +0100

    moved extensions from RFC1345 to defaults/preferences

--- digraphs@boldewyn.de/defaults/preferences/digraphs.js
+++ digraphs@boldewyn.de/defaults/preferences/digraphs.js
@@ -1,3 +1,10 @@
 /* default activation is Ctrl-K */
 pref("extensions.digraphs.activate_key", 107);
 pref("extensions.digraphs.activate_mod", "ctrlKey");
+
+/* extensions to RFC 1345 */
+/* Euro */
+pref("extensions.digraphs.custom.Eu", 8364);
+pref("extensions.digraphs.custom.=e", 8364);
+/* Ellipsis */
+pref("extensions.digraphs.custom.,:", 8230);
